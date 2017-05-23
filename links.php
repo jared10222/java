083bd8074fee41d048b32329af440cb9dc3154f1
Jared Burdick <jared_burdick10@yahoo.com>
2017-05-22 22:58:08 -0500

    Images added

--- links.php
+++ links.php
@@ -13,7 +13,7 @@ require_once("includes/header.php");
   <ul class="list-group">
     <li class="list-group-item"><a href="https://www.edx.org/course/introduction-computer-science-harvardx-cs50x#!" target="_blank">Introduction to Computer Science - Harvard</a></li>
     <li class="list-group-item"><a href="https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/" target="_blank">Adding an existing project to GitHub using the command line</a></li>
-    <li class="list-group-item"><a href="" target="_blank"></a></li>
+    <li class="list-group-item"><a href="https://scratch.mit.edu/" target="_blank">Scratch</a></li>
     <li class="list-group-item"><a href="" target="_blank"></a></li>
   </ul>
 
--- notes/introduction-to-computer-science-harvard/intro.php
+++ notes/introduction-to-computer-science-harvard/intro.php
@@ -1,2 +1,48 @@
 <h2><?php echo formatName($_GET['section']); ?></h2>
 <hr />
+<figure style="max-width:100%; width: 500px;">
+<img class="img-thumbnail img-fluid" src="images/unary.png" alt="Unary - Tally Marks" />
+<figcaption>
+<p>This is called <b>unary</b>, where there is one symbol and you simply
+  add another one for each thing you're counting.</p>
+</figcaption>
+</figure>
+<hr />
+<dl>
+  <dt>Bit</dt>
+  <dd>A <b>bit</b> is a value of either a 1 of 0 (on or off)</dd>
+  <dt>Nibble</dt>
+  <dd>A <b>Nibble</b> is 4 bits</dd>
+  <dt>Byte</dt>
+  <dd>Today, a <b>Byte</b> is 8 bits</dd>
+  <dt>Kilobyte (KB)</dt>
+  <dd>A <b>Kilobyte</b> is 1,024 bytes</dd>
+  <dt>Megabyte (MB)</dt>
+  <dd>A <b>Megabyte</b> is 1,048,576 bytes or 1,024 kilobytes</dd>
+  <dt>Gigabyte (GB)</dt>
+  <dd>A <b>Gigabyte</b> is 1,073,741,824 (2<sup>30</sup>) bytes.
+  1,024 Megabytes, or 1,048,576 kilobytes.</dd>
+  <dt>Terabyte (TB)</dt>
+  <dd>A <b>Terabyte</b> is 1,099,511,627,776 (2<sup>40</sup>) bytes, 1,024 Gigabytes
+  or 1,048,576 Megabytes.</dd>
+  <dt>Petabyte (PB)</dt>
+  <dd>A <b>Petabyte</b> is 1,125,899,906,842,624 (2<sup>50</sup>) bytes,
+  1,024 Terabytes, 1,048,576 Gigabytes, or 1,073,741,824 Megabytes.</dd>
+  <dt>Exabyte (EB)</dt>
+  <dd>A <b>Exabyte</b> is 1,152,921,504,606,846,976 (2<sup>60</sup>) bytes,
+  1,024 Petabytes, 1,048,576 Terabytes, 1,073,741,824 Gigabytes, or
+1,099,511,627,776 Megabytes.</dd>
+  <dt>Zettabyte (ZB)</dt>
+  <dd>A <b>Zettabyte</b> is 1,180,591,620,717,411,303,424 (270) bytes,
+    1,024 Exabytes, 1,048,576 Petabytes, 1,073,741,824 Terabytes,
+    1,099,511,627,776 Gigabytes, or 1,125,899,910,000,000 Megabytes.</dd>
+  <dt>Yottabyte (YB)</dt>
+  <dd>A <b>Yottabyte</b> is 1,208,925,819,614,629,174,706,176 (280) bytes,
+     1,024 Zettabytes, 1,048,576 Exabytes, 1,073,741,824 Petabytes,
+     1,099,511,627,776 Terabytes, 1,125,899,910,000,000 Gigabytes, or
+     1,152,921,500,000,000,000 Megabytes.</dd>
+</dl>
+
+<hr />
+<h3>Algorithms</h3>
+<p><b>Algorithms</b> are sets of instructions to solve particular problems.</p>
